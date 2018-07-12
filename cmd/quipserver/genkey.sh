fe5bd84d6f1cbaf453431c23e0cabf4e3a25226d
Steven E. Newton <cratermoon@gmail.com>
2018-07-11 20:45:44 -0700

    different keygen method

--- cmd/quipserver/genkey.sh
+++ cmd/quipserver/genkey.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-openssl req -x509 -nodes -newkey rsa:4096 -keyout $1.crt -out $1.key -subj '/C=OR/ST=Oregon/L=Portland/O=Crater Moon Development/CN=cmdev.com/emailAddress=steven.e.newton@gmail.com'
+openssl req -x509 -nodes -sha256 -newkey rsa:4096 -keyout $1.key -out $1.crt -subj '/C=OR/ST=Oregon/L=Portland/O=Crater Moon Development/CN=cmdev.com/emailAddress=steven.e.newton@gmail.com'
